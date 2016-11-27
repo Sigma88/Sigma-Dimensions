e98a03fc073e04861e3b4a3dbc8b0d94890b1f5d
Sigma88 <sigmamodexpansions@outlook.com>
2016-11-27 17:17:01 +0100

    readd VS stuff

---
+++ [Source]/SigmaDimensions/SigmaDimensions.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using Kopernicus;
+using Kopernicus.Components;
+using Kopernicus.Configuration;
+using Kopernicus.OnDemand;
+using System.Text;
+using System;
+using System.Reflection;
+using System.Linq;
+using KSP.UI.Screens;
+
+
+namespace SigmaDimensionsPlugin
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class SigmaDimensions : MonoBehaviour
+    {
+        public double resize = 1;
+        public double landscape = 1;
+        public double resizeBuildings= 1;
+        public CelestialBody body = null;
+
+        void Start()
+        {
+            foreach (CelestialBody cb in FlightGlobals.Bodies)
+            {
+                body = cb;
+                resize = body.Has("resize") ? body.Get<double>("resize") : 1;
+                landscape = body.Has("landscape") ? body.Get<double>("landscape") : 1;
+                resizeBuildings = body.Has("resizeBuildings") ? body.Get<double>("resizeBuildings") : 1;
+
+                foreach (PQSSurfaceObject obj in body.pqsSurfaceObjects)
+                {
+                    PQSCity pqscity = obj.GetComponent<PQSCity>();
+                    if (pqscity != null)
+                    {
+                        if (pqscity.name == "KSC")
+                            KSCFixer(pqscity);
+                        else
+                            CityFixer(pqscity);
+                    }
+
+                    PQSCity2 pqscity2 = obj.GetComponent<PQSCity2>();
+                    if (pqscity2 != null)
+                        City2Fixer(pqscity2);
+                }
+            }
+        }
+
+        void KSCFixer(PQSCity pqs)
+        {
+            pqs.repositionToSphereSurface = true;
+            pqs.repositionToSphereSurfaceAddHeight = false;
+            if (resizeBuildings != 1)
+            {
+                // Resize the Building
+
+                pqs.transform.localScale *= (float)resizeBuildings;
+            }
+        }
+
+        void CityFixer(PQSCity pqs)
+        {
+            if (!pqs.repositionToSphere && !pqs.repositionToSphereSurface)
+            {
+                // Offset = Distance from the center of the planet
+
+                double fromRadius = pqs.repositionRadiusOffset - (body.Radius / resize);
+                pqs.repositionRadiusOffset = fromRadius * resize * landscape + body.Radius;
+            }
+            if (pqs.repositionToSphere && !pqs.repositionToSphereSurface)
+            {
+                // Offset = Distance from the radius of the planet
+
+                pqs.repositionRadiusOffset *= resize * landscape;
+            }
+            if (pqs.repositionToSphereSurface && pqs.repositionToSphereSurfaceAddHeight && resizeBuildings != 1)
+            {
+                // Offset = Distance from the surface of the planet
+
+                pqs.repositionRadiusOffset *= resizeBuildings;
+            }
+            if (resizeBuildings != 1)
+            {
+                // Resize the Building
+
+                pqs.transform.localScale *= (float)resizeBuildings;
+            }
+        }
+        
+        void City2Fixer(PQSCity2 pqs)
+        {
+            if (!pqs.snapToSurface)
+            {
+                // Offset = Distance from the center of the planet
+
+                double fromRadius = pqs.snapHeightOffset - (body.Radius / resize);
+                pqs.snapHeightOffset = fromRadius * resize * landscape + body.Radius;
+            }
+            else if (resizeBuildings != 1)
+            {
+                // Offset = Distance from the surface of the planet
+
+                pqs.snapHeightOffset *= resizeBuildings;
+            }
+            if (resizeBuildings != 1)
+            {
+                // Resize the Building
+
+                pqs.transform.localScale *= (float)resizeBuildings;
+            }
+        }
+    }
+}
---
+++ [Source]/SigmaDimensions/SigmaDimensionsLoader.cs
@@ -0,0 +1,48 @@
+using Kopernicus;
+using Kopernicus.Configuration;
+
+namespace SigmaDimensionsPlugin
+{
+    [ExternalParserTarget("SigmaDimensions")]
+    public class SigmaDimensionsLoader : ExternalParserTargetLoader, IParserEventSubscriber
+    {
+        [ParserTarget("Resize", optional = true)]
+        public NumericParser<double> resize
+        {
+            set
+            {
+                generatedBody.celestialBody.Set("resize", value.value);
+            }
+        }
+
+        [ParserTarget("landscape", optional = true)]
+        public NumericParser<double> landscape
+        {
+            set
+            {
+                generatedBody.celestialBody.Set("landscape", value.value);
+            }
+        }
+
+        [ParserTarget("resizeBuildings", optional = true)]
+        public NumericParser<double> resizeBuildings
+        {
+            set
+            {
+                generatedBody.celestialBody.Set("resizeBuildings", value.value);
+            }
+        }
+
+        void IParserEventSubscriber.Apply(ConfigNode node)
+        {
+        }
+
+        void IParserEventSubscriber.PostApply(ConfigNode node)
+        {
+        }
+
+        public SigmaBinaryLoader()
+        {
+        }
+    }
+}
