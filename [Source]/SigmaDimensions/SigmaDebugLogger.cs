bd5bdadd49eb51a6c02848e4e58afcdce62f4376
Sigma88 <sigmamodexpansions@outlook.com>
2016-12-10 18:30:20 +0100

    new debug functionality

---
+++ [Source]/SigmaDimensions/SigmaDebugLogger.cs
@@ -0,0 +1,12 @@
+﻿namespace SigmaDimensionsPlugin
+{
+    public static class Debug
+    {
+        public static bool debug = false;
+
+        public static void Log(string s)
+        {
+            if (debug) UnityEngine.Debug.Log("SigmaLog: " + s);
+        }
+    }
+}
