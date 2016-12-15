a5b8904f4b3153aff85121674ca00038a5e4390c
Sigma88 <sigmamodexpansions@outlook.com>
2016-12-15 17:29:20 +0100

    I hate vectors
    Vector3 == 💩

--- [Source]/SigmaDimensions/SigmaDimensions.cs
+++ [Source]/SigmaDimensions/SigmaDimensions.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Kopernicus;
-using Kopernicus.Configuration;
-using SigmaDimensionsPlugin;
 
 
 namespace SigmaDimensionsPlugin
@@ -142,36 +140,54 @@ namespace SigmaDimensionsPlugin
 
             Vector3 PQSvector = pqs.PlanetRelativePosition.normalized;
             Vector3 NEWvector = Vector3.LerpUnclamped(REFvector, PQSvector, (float)(resizeBuildings / resize));
-            double[] LLA = ECEFtoLLA(NEWvector);
-            pqs.lat = LLA[0];
-            pqs.lon = LLA[1];
+            LatLon NEWlatlon = new LatLon(NEWvector);
+            pqs.lat = NEWlatlon.lat;
+            pqs.lon = NEWlatlon.lon;
         }
 
         void LinkToKSC(PQSCity pqs)
         {
             PQSCity KSC = body.GetComponentsInChildren<PQSCity>().First(m => m.name == "KSC");
-            Vector3 movedKSC = KSC.repositionRadial.normalized;
+            LatLon movedKSC = new LatLon(KSC.repositionRadial.normalized);
+            Vector3 reference = body.Get<Dictionary<object, Vector3>>("PQSCityGroups")[pqs].normalized;
 
-            if (body.Get<Dictionary<object, Vector3>>("PQSCityGroups")[pqs].normalized == movedKSC)
+            if (reference == movedKSC.vector)
             {
                 // Fix Rotation
                 float angle = KSC.reorientFinalAngle - (-15);
                 pqs.reorientFinalAngle += angle;
 
+                // Stock Vectors (KSC, North, East)
+                LatLon stockKSC = new LatLon(new Vector3(157000, -1000, -570000).normalized);
+                Vector3 north = Vector3.ProjectOnPlane(Vector3.up, stockKSC.vector);
+                Vector3 east = QuaternionD.AngleAxis(90, stockKSC.vector) * north;
 
-                // Fix Latitude and Longitude
-                Vector3 stockKSC = new Vector3(157000, -1000, -570000).normalized;
-                double dLON = (Math.Atan2(movedKSC.z, movedKSC.x) - Math.Atan2(stockKSC.z, stockKSC.x)) * 180 / Math.PI;
-                Quaternion rotation = Quaternion.AngleAxis((float)dLON + angle, movedKSC);
+                // PQS Vectors (PQS, North, East)
+                Vector3 oldPQS = Vector3.ProjectOnPlane(pqs.repositionRadial.normalized, stockKSC.vector);
+                Vector3 pqsNorth = Vector3.Project(oldPQS, north);
+                Vector3 pqsEast = Vector3.Project(oldPQS, east);
 
-                pqs.repositionRadial = pqs.repositionRadial.normalized + movedKSC - stockKSC;
-                pqs.repositionRadial = rotation * pqs.repositionRadial.normalized;
+                // Distance from KSC (Northward, Eastward)
+                float northward = pqsNorth.magnitude * (1 - (Vector3.Angle(north.normalized, pqsNorth.normalized) / 90));
+                float eastward = pqsEast.magnitude * (1 - (Vector3.Angle(east.normalized, pqsEast.normalized) / 90));
 
+                // New KSC Vectors (North, East)
+                Vector3 newNorth = Vector3.ProjectOnPlane(Vector3.up, movedKSC.vector).normalized;
+                Vector3 newEast = (QuaternionD.AngleAxis(90, movedKSC.vector) * newNorth);
+
+                // Account for PQSCity rotation:
+                // PQSCity rotate when their Longitude changes
+                angle -= (float)(stockKSC.lon - movedKSC.lon);
+                QuaternionD rotation = QuaternionD.AngleAxis(angle, movedKSC.vector);
+
+                // Calculate final position by adding the north and east distances to the movedKSC position
+                // then rotate the new vector by as many degrees as it is necessary to account for the PQS model rotation
+                pqs.repositionRadial = rotation * (movedKSC.vector + newNorth * northward + newEast * eastward);
 
                 // Fix Altitude
                 if (!pqs.repositionToSphereSurface)
                 {
-                    pqs.repositionRadiusOffset += (body.pqsController.GetSurfaceHeight(movedKSC) - body.Radius) / (resize * landscape) - 64.7846885412;
+                    pqs.repositionRadiusOffset += (body.pqsController.GetSurfaceHeight(movedKSC.vector) - body.Radius) / (resize * landscape) - 64.7846885412;
                 }
             }
         }
@@ -179,38 +195,113 @@ namespace SigmaDimensionsPlugin
         void LinkToKSC(PQSCity2 pqs)
         {
             PQSCity KSC = body.GetComponentsInChildren<PQSCity>().First(m => m.name == "KSC");
-            Vector3 movedKSC = KSC.repositionRadial.normalized;
+            LatLon movedKSC = new LatLon(KSC.repositionRadial.normalized);
+            Vector3 reference = body.Get<Dictionary<object, Vector3>>("PQSCityGroups")[pqs].normalized;
 
-            if (body.Get<Dictionary<object, Vector3>>("PQSCityGroups")[pqs].normalized == movedKSC)
+            if (reference == movedKSC.vector)
             {
                 // Fix Rotation
                 float angle = KSC.reorientFinalAngle - (-15);
                 pqs.rotation += angle;
 
+                // Stock Vectors (KSC, North, East)
+                LatLon stockKSC = new LatLon(new Vector3(157000, -1000, -570000).normalized);
+                Vector3 north = Vector3.ProjectOnPlane(Vector3.up, stockKSC.vector);
+                Vector3 east = QuaternionD.AngleAxis(90, stockKSC.vector) * north;
 
-                // Fix Latitude and Longitude
-                Vector3 stockKSC = new Vector3(157000, -1000, -570000).normalized;
-                double dLON = (Math.Atan2(movedKSC.z, movedKSC.x) - Math.Atan2(stockKSC.z, stockKSC.x)) * 180 / Math.PI;
-                Quaternion rotation = Quaternion.AngleAxis((float)dLON + angle, movedKSC);
-                Vector3 vector = pqs.PlanetRelativePosition;
-                vector = vector.normalized + movedKSC - stockKSC;
-                vector = rotation * vector.normalized;
+                // PQS Vectors (PQS, North, East)
+                Vector3 oldPQS = Vector3.ProjectOnPlane(pqs.PlanetRelativePosition.normalized, stockKSC.vector);
+                Vector3 pqsNorth = Vector3.Project(oldPQS, north);
+                Vector3 pqsEast = Vector3.Project(oldPQS, east);
 
+                // Distance from KSC (Northward, Eastward)
+                float northward = pqsNorth.magnitude * (1 - (Vector3.Angle(north.normalized, pqsNorth.normalized) / 90));
+                float eastward = pqsEast.magnitude * (1 - (Vector3.Angle(east.normalized, pqsEast.normalized) / 90));
+
+                // New KSC Vectors (North, East)
+                Vector3 newNorth = Vector3.ProjectOnPlane(Vector3.up, movedKSC.vector).normalized;
+                Vector3 newEast = (QuaternionD.AngleAxis(90, movedKSC.vector) * newNorth);
+
+                // Account for PQSCity rotation:
+                // PQSCity rotate when their Longitude changes
+                angle -= (float)(stockKSC.lon - movedKSC.lon);
+                QuaternionD rotation = QuaternionD.AngleAxis(angle, movedKSC.vector);
+
+                // Calculate final position by adding the north and east distances to the movedKSC position
+                // then rotate the new vector by as many degrees as it is necessary to account for the PQS model rotation
+                LatLon newPQS = new LatLon(rotation * (movedKSC.vector + newNorth * northward + newEast * eastward));
+                pqs.lat = newPQS.lat;
+                pqs.lon = newPQS.lon;
 
                 // Fix Altitude
                 if (!pqs.snapToSurface)
                 {
-                    pqs.snapHeightOffset += (body.pqsController.GetSurfaceHeight(movedKSC) - body.Radius) / (resize * landscape) - 64.7846885412;
+                    pqs.alt += (body.pqsController.GetSurfaceHeight(movedKSC.vector) - body.Radius) / (resize * landscape) - 64.7846885412;
                 }
             }
         }
 
-        public double[] ECEFtoLLA(Vector3 vector)
+        public class LatLon
         {
-            double lat = 90 + Math.Atan2(-vector.z / Math.Sin(Math.Atan2(vector.z, vector.x)), vector.y) * 180 / Math.PI;
-            double lon = Math.Atan2(vector.z, vector.x) * 180 / Math.PI;
-            double alt = Math.Pow(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z, 0.5);
-            return new double[] { lat, lon, alt };
+            double[] data = { 1, 1, 1 };
+            Vector3 v = Vector3.one;
+
+            public double lat
+            {
+                get { return data[0]; }
+                set
+                {
+                    data[0] = value;
+                    Update();
+                }
+            }
+            public double lon
+            {
+                get { return data[1]; }
+                set
+                {
+                    data[1] = value;
+                    Update();
+                }
+            }
+            public double alt
+            {
+                get { return data[2]; }
+                set
+                {
+                    data[2] = value;
+                    Update();
+                }
+            }
+            public Vector3 vector
+            {
+                get { return v; }
+                set
+                {
+                    v = value;
+                    data[0] = 90 + Math.Atan2(-v.z / Math.Sin(Math.Atan2(v.z, v.x)), v.y) * 180 / Math.PI;
+                    data[1] = Math.Atan2(v.z, v.x) * 180 / Math.PI;
+                    data[2] = Math.Pow(v.x * v.x + v.y * v.y + v.z * v.z, 0.5);
+                }
+            }
+            void Update()
+            {
+                v = Utility.LLAtoECEF(data[0], data[1], 0, data[2]);
+            }
+            public LatLon() //LLA()
+            {
+            }
+            public LatLon(Vector3 input)//LLA(Vector3 input)
+            {
+                vector = input;
+            }
+            public LatLon(LatLon input)//LLA(LatLon input)
+            {
+                data[0] = input.lat;
+                data[1] = input.lon;
+                data[2] = input.alt;
+                v = input.vector;
+            }
         }
     }
 }
