532dbca18d20399cdc5c4e1fdd52fe0e1ccac3ac
Sigma88 <sigmamodexpansions@outlook.com>
2017-07-01 02:11:08 +0200

    add noderemover

---
+++ [Source]/SigmaDimensions/NodeRemover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Linq;
+
+
+namespace SigmaDimensionsPlugin
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    class ConfigNodeRemover : MonoBehaviour
+    {
+        void Start()
+        {
+            foreach (UrlDir.UrlConfig node in GameDatabase.Instance.GetConfigs("SigmaDimensions").Where(c => c.url != "Sigma/Dimensions/Settings/SigmaDimensions"))
+            {
+                node.parent.configs.Remove(node);
+            }
+        }
+    }
+}
