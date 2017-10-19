a8687ce6b554d4966aab7ba350e092e2361524f5
Sigma88 <sigmamodexpansions@outlook.com>
2017-10-19 02:52:28 +0200

    improve version and noderemover

--- [Source]/SigmaDimensions/NodeRemover.cs
+++ [Source]/SigmaDimensions/NodeRemover.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using UnityEngine;
 
 
@@ -7,6 +8,35 @@ namespace SigmaDimensionsPlugin
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     class ConfigNodeRemover : MonoBehaviour
     {
+        void Awake()
+        {
+            string folder = "GameData/Sigma/Dimensions/";
+            string file = "Settings";
+            string node = "SigmaDimensions";
+
+
+            if (!Directory.Exists(folder))
+            {
+                UnityEngine.Debug.Log("[SigmaLog] WARNING: Missing folder => " + folder);
+                return;
+            }
+
+            if (!File.Exists(folder + file + ".cfg"))
+            {
+                UnityEngine.Debug.Log("[SigmaLog] WARNING: Missing file => " + folder + file + ".cfg");
+
+                File.WriteAllLines(folder + file + ".cfg", new[] { node + " {}" });
+                return;
+            }
+
+            if (ConfigNode.Load(folder + file + ".cfg")?.HasNode("SigmaDimensions") != true)
+            {
+                UnityEngine.Debug.Log("[SigmaLog] WARNING: Missing node => " + folder + file + "/" + node);
+
+                File.AppendAllText(folder + file + ".cfg", "\r\n" + node + " {}");
+            }
+        }
+
         void Start()
         {
             foreach (UrlDir.UrlConfig node in GameDatabase.Instance.GetConfigs("SigmaDimensions").Where(c => c.url != "Sigma/Dimensions/Settings/SigmaDimensions"))
