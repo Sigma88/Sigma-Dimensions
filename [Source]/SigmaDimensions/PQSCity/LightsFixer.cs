f0a9255c7eb6e4d49db7b1a73842dfa51b6ab082
Sigma88 <sigmamodexpansions@outlook.com>
2019-06-17 21:02:40 +0200

    clean up code

--- [Source]/SigmaDimensions/PQSCity/LightsFixer.cs
+++ [Source]/SigmaDimensions/PQSCity/LightsFixer.cs
@@ -26,21 +26,4 @@ namespace SigmaDimensionsPlugin
             }
         }
     }
-
-    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
-    internal class SpaceCenterFixer : MonoBehaviour
-    {
-        void Start()
-        {
-            foreach (SpaceCenterCamera2 camera in Resources.FindObjectsOfTypeAll<SpaceCenterCamera2>())
-            {
-                float resizeBuildings = (float)FlightGlobals.GetHomeBody().Get<double>("resizeBuildings");
-
-                camera.zoomInitial *= resizeBuildings;
-                camera.zoomMax *= resizeBuildings;
-                camera.zoomMin *= resizeBuildings;
-                camera.zoomSpeed *= resizeBuildings;
-            }
-        }
-    }
 }
